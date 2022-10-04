d4d9f1168f2fb71f086d9d9ea18c9d5d9f8ac5f2
William Nunes Ferreira <william.ferreira@outlook.com.br>
2022-10-04 10:52:21 -0300

    front-ready2deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:'https://localhost:5001/',
+    baseURL:'https://api-studychat.azurewebsites.net/',
 }); 
 
 export default api;
